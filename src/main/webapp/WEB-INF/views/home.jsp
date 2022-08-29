1bd6b67ea2a832b1369cfc954f1839d60df3cdcc
jihea <melonar@naver.com>
2022-08-29 16:49:18 +0900

    commit

--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -1,5 +1,4 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-<script src="${contextPath }/resources/js/main.js"></script>
 
 <body>
 <div class="outer">
@@ -25,5 +24,5 @@
 	<input class="btn3" type="radio" name="banner-btn">
 </div>
 
-
 </body>
+<script src="resources/js/main.js"></script>
--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -1,15 +1,22 @@
-
 const container = document.querySelector('.container')
 const btn1 = document.querySelector('.btn1');
 const btn2 = document.querySelector('.btn2');
 const btn3 = document.querySelector('.btn3');
+const leftBtn = document.querySelector('.left-btn');
+const rightBtn = document.querySelector('.right-btn');
+let index = 0;
 
 btn1.addEventListener('click', function(){
-	container.style.transform = 'translate(0vw)'
+	container.style.transform = `translate(index * -80vw)`;
+	index++;
 })
 btn2.addEventListener('click', function(){
 	container.style.transform = 'translate(-80vw)'
 })
 btn3.addEventListener('click', function(){
 	container.style.transform = 'translate(-160vw)'
+})
+
+leftBtn.addEventListener('click', function(){
+	container.style.transform = 'translate'
 })
