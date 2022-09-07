311cdda5bd4fa5dfdf2f157865805f2a83798313
hhh <test@a.a>
2022-09-07 21:49:23 +0900

    review

--- src/main/java/com/kg/seeot/board/controller/ReviewRepController.java
+++ src/main/java/com/kg/seeot/board/controller/ReviewRepController.java
@@ -1,5 +1,6 @@
 package com.kg.seeot.board.controller;
 
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,6 +9,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,19 +25,7 @@ import com.kg.seeot.board.service.ReviewService;
 public class ReviewRepController {
 	@Autowired ReviewService rs;
 	
-	/*
-	@PostMapping(value="addReview",produces="application/json;charset=utf8" )
-	public void addReview(@RequestParam(value="memberId", required=false) String memberId,
-			@RequestParam(value="reviewContent", required=false) String reviewContent, 
-				@RequestParam(value="reviewStar", required=false) String reviewStar) {
-		System.out.println("controller");
-		System.out.println(memberId);
-		System.out.println(reviewContent);
-		System.out.println(reviewStar);
-		//rs.addReview( memberId,reviewContent, reviewStar);
-	}
-	*/
-	//map<key,value>
+	
 	@PostMapping(value="addReview",produces="application/json;charset=utf8" )
 	public void addReview(@RequestBody Map<String, Object> map) {
 		System.out.println("controller"); 
@@ -45,20 +35,27 @@ public class ReviewRepController {
 		  System.out.println((String)map.get("reviewFile"));
 		  System.out.println(map.get("productNo"));
 		  System.out.println(map.get("reviewStar")); 
-		  System.out.println(map.get("rating")); 
-		
 		
 		//int reviewStar = Integer.parseInt(String.valueOf(map.get("reviewStar")) );
 	
-		
-	
-		rs.addReview(map);
+		ReviewDTO dto = new ReviewDTO();
+		dto.setMemberId((String)map.get("memberId"));
+		dto.setReviewContent((String)map.get("reviewContent"));
+		dto.setReviewFile((String)map.get("reviewFile"));
+		dto.setProductNo(Integer.parseInt(String.valueOf(map.get("productNo"))) );
+		dto.setReviewStar(Integer.parseInt(String.valueOf(map.get("reviewStar")) ));
+		rs.addReview(dto);
 	}
 	
-	@PostMapping(value="addStar",produces="application/json;charset=utf8")
-	public void addStar(int reviewStar) {
-		rs.addStar(reviewStar);
+	
+	
+	@GetMapping(value = "replyData/{productNo}", produces="application/json;charset=utf8")
+	public List<ReviewDTO> replyData(
+						@PathVariable int productNo){
+		return rs.getRepList(productNo);
 	}
+	
+	
     /*
    
      * 게시물 댓글 불러오기(Ajax)
--- src/main/java/com/kg/seeot/board/service/ReviewService.java
+++ src/main/java/com/kg/seeot/board/service/ReviewService.java
@@ -1,5 +1,6 @@
 package com.kg.seeot.board.service;
 
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -7,12 +8,15 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.ui.Model;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import com.kg.seeot.board.dto.ReviewDTO;
+
 public interface ReviewService {
 	public void reviewList(Model model);
 	//public String writeSave(MultipartHttpServletRequest mul,
 	//		HttpServletRequest request);
 	public void addReply(Map<String, String>map, String memberId);
-	public void addReview(Map<String,Object> map );
-	public void addStar(int reviewStar);
+	public void addReview(ReviewDTO dto);
+	public  List<ReviewDTO> getRepList(int productNo);
+	
 
 }
