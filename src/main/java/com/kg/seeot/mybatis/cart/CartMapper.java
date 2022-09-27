e7ff48fdbcdc421d77e768d5554a334907b376de
hhh <twks0513@gmail.com>
2022-09-27 21:48:02 +0900

    0927 css적용 및 버그수정

--- src/main/java/com/kg/seeot/mybatis/cart/CartMapper.java
+++ src/main/java/com/kg/seeot/mybatis/cart/CartMapper.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.annotations.Param;
 import com.kg.seeot.cart.dto.CartDTO;
 
 public interface CartMapper {
-	public void addCart_p(@Param("n") int productNo,@Param("s") String productSize, @Param("c") String productColor,@Param("o") String productSatck);
+	public void addCart_p(@Param("m")String memberId,@Param("n") int productNo,@Param("s") String productSize, @Param("c") String productColor,@Param("o") String productSatck);
 	public void addCart_m(@Param("n") int productNo,@Param("s") String productSize, @Param("c") String productColor);
 	public ArrayList<CartDTO> getCart(String memberId);
 	public int deleteCartOne(@Param("m") String memberId, @Param("p") int productNo);
