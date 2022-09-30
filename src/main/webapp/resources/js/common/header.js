71309b4c7d6d154d7e8e68d5c2ea551ebd31780e
jihea <melonar@naver.com>
2022-09-30 16:23:58 +0900

    메뉴바 언더라인 js 수정

--- src/main/webapp/resources/js/common/header.js
+++ src/main/webapp/resources/js/common/header.js
@@ -3,17 +3,23 @@
  */
 let underline = document.querySelector('.underline');
 let category = document.querySelectorAll('.category > li > a > span');
+let leftArea = document.querySelector('.category')
 
 category.forEach((menu) => 
-	menu.addEventListener('mouseover', (e) => indicator(e))
+	menu.addEventListener('mouseover', (e) => makeLine(e))
 );
 
-function indicator(e){
+leftArea.addEventListener('mouseleave', () => underline.style.display = 'none');
+
+
+function makeLine(e){ 
+	underline.style.display = 'block';
 	underline.style.left = e.currentTarget.offsetLeft + 'px';
 	underline.style.width = e.currentTarget.offsetWidth + 'px';
 	underline.style.top = 
 		e.currentTarget.offsetTop + e.currentTarget.offsetHeight + 'px';
 }
+
 
 /**
  * 서치 모달창 스크립트
